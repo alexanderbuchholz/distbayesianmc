77c2438c144ae8d5ba76d9bd1005ef5ef83f9e3e
alexanderbuchholz <alexanderbuchholz@yahoo.de>
2019-07-11 16:48:35 +0200

    add sparse reg

---
+++ params_simulation/params_sparse.R
@@ -0,0 +1,26 @@
+# params simulation linear model with laplace prior
+
+# specifics for stan
+fileName <- "~/R_programming/distbayesianmc/stan_files/sparse_linear_reg_laplace.stan"
+
+# single simulation
+
+scale <- 1
+
+#dataset <- "higgs1"
+#iseed <- 42
+#iter <- 1
+#ssplits <- 5
+
+typesplit <-  "random"
+nobs <- 4*10**3
+nchain <- 2000
+typeprior <- "laplace_normal"
+
+iters <- 10
+vec_datasets <- c("sparse_reg_1", "sparse_reg_2")
+#vec_datasets <- c(dataset)
+vec_types_splits <- c(typesplit)
+
+# multiple simulations
+vec_splits <- c(1,2,3,5,10,20,50,100)
