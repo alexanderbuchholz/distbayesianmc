f489a0698dac9810cadd8c3fa59384c78d04e25d
Alexander Buchholz <alexander.buchholz@ensae.fr>
2019-07-31 17:56:27 +0100

    try to make rjmcmc work

--- R2BGLiMS/try_R2BGLiMS.R
+++ R2BGLiMS/try_R2BGLiMS.R
@@ -2,70 +2,100 @@
 library(R2BGLiMS)
 library(distbayesianmc)
 
-dataset_loaded <- f_dataset_loader("pima")
-pvars <- dim(dataset_loaded$X)[2]
-#pvars <- 4
-#splitted_data <- f_pack_split_data(dataset_loaded$X, dataset_loaded$y, ssplits=ssplits, iseed=iter, typesplit="random")
-X <-  dataset_loaded$X[,2:pvars]
-predictors <- colnames(X)
-
-data <- cbind(X, dataset_loaded$y)
-colnames(data)[pvars] <- "outcome"
-
-beta.priors <- cbind(rep(0, pvars-1), rep(1, pvars-1))
-row.names(beta.priors) <- predictors
-
-pima.results <- R2BGLiMS(
-      likelihood="Logistic",
-      data=data,
-      outcome.var="outcome",
-      g.prior = FALSE,
-      model.space.priors=list(
-          "a"=1, "b"=1, "Variables"=predictors),
-      n.mil=5,
-      seed=2,
-      thinning.interval = 1,
-      #beta.prior.partitions = list(list("Variables" = predictors, "UniformA"=0.9999, "UniformB"=1)), # fix the hyper prior
-      beta.priors = beta.priors,
-      extra.arguments = list("Adaption_Iterations" = 2e5, 
-                             "AlphaPriorSd" = 1
-                             )
+f_rjmcmc_on_splits <- function(splitted_data_rjmcmc, i_split = 1, n.mil = 2, i_seed=1){
+  # bringing the data into the right format
+  pvars <- splitted_data_rjmcmc[[i_split]]$Pparams
+  X <-  splitted_data_rjmcmc[[i_split]]$X[,2:pvars]
+  predictors <- colnames(X)
+  data <- cbind(X, splitted_data_rjmcmc[[i_split]]$y)
+  colnames(data)[pvars] <- "outcome"
+  # preparing the betaprior
+  # TODO: make sur that the scale is set right here!
+  beta.priors <- cbind(rep(0, pvars-1), rep(splitted_data_rjmcmc[[i_split]]$scale, pvars-1))
+  row.names(beta.priors) <- predictors
+  
+  rjmcmc.results <- R2BGLiMS(
+    likelihood="Logistic",
+    data=data,
+    outcome.var="outcome",
+    g.prior = FALSE,
+    model.space.priors=list(
+      "a"=1, "b"=1, "Variables"=predictors),
+    n.mil=n.mil,
+    seed=i_seed,
+    thinning.interval = 1,
+    beta.priors = beta.priors,
+    extra.arguments = list("Adaption_Iterations" = 2e5, 
+                           "AlphaPriorSd" = splitted_data_rjmcmc[[i_split]]$scale**0.5
     )
-topmodel_res <- TopModels(pima.results)
-ManhattanPlot(pima.results)
-pima.results@mcmc.output
-
-#pima.results@posterior.summary.table
-
-# count_full_collumns <- function(x){
-#   all(!(x  == 0))
-# }
-# 
-# count_collumns_not_bp_skin <- function(x){
-#   selector_small <-  !(colnames(x) %in% c("bp", "skin", "age"))
-#   return(all(!(x[selector_small]  == 0)) & all(x[!selector_small]  == 0))
-# }
-
-
-#count_full_collumns(pima.results@mcmc.output[9,])
-#count_collumns_not_bp_skin(pima.results@mcmc.output[235,])
-
-#mean(apply(pima.results@mcmc.output, 1, count_full_collumns))
-#mean(apply(pima.results@mcmc.output, 1, count_collumns_not_bp_skin))
-
-prior_specific_model <- function(dim_model, dim_all, a, b){
+  )
+  topmodel_res <- TopModels(rjmcmc.results)  
+  mcmc_ouput <- rjmcmc.results@mcmc.output  
+  return(list(topmodel_res=topmodel_res, mcmc_ouput=mcmc_ouput))
+}
+  
+prior_prob_k_specific_model <- function(dim_model, dim_all, a=1, b=1){
+  # gives the prior probability of a model with a specific dimension, given the full dimension and a and b
   part1 <- beta(dim_model+a, dim_all - dim_model+b)
   part2 <- (dim_all+1)*beta(dim_model+1, dim_all - dim_model+1)*beta(a, b)
   return(part1/(part2 * choose(dim_all,dim_model))) # take into account that we are looking at exactly one single model
 }
 
+f_filter_rows_mcmc_rjmcmc_output <- function(mcmc_ouput, topmodel_res, index_model){
+  # filter selected colums
+  pvars <- dim(topmodel_res)[2]
+  selector_model <- c(1, topmodel_res[index_model,(1:pvars-1)])==1
+  mcmc_ouput_inter <- mcmc_ouput[,1:pvars]
+  filter_matrix <- mcmc_ouput_inter != 0
+  selector_rows <- apply(filter_matrix, 1, function(x) all(x==selector_model))
+  #selector_rows <- filter_matrix %*% selector_model == sum(selector_mode)
+  return(mcmc_ouput_inter[selector_rows, selector_model])
+}
 
-indexM1 <- 1
-indexM2 <- 4
-log(topmodel_res[indexM1,pvars]/topmodel_res[indexM2,pvars])
+f_normal_approx_rjmcmc_output <- function(mcmc_ouput, topmodel_res, index_model){
+  # first filter the output
+  mcmc_ouput_inter <- f_filter_rows_mcmc_rjmcmc_output(mcmc_ouput, topmodel_res, index_model)
+  # calculate means on the filtered ouput
+  modelmeans <- colMeans(mcmc_ouput_inter)
+  modelcovars <- cov(mcmc_ouput_inter)
+  return(list(modelmeans=modelmeans, modelcovars=modelcovars))
+}
 
-selectorM1 <- c(1, topmodel_res[indexM1,(1:pvars-1)])
-selectorM2 <- c(1, topmodel_res[indexM2,(1:pvars-1)])
+
+f_bayesfactor_two_models_rjmcmc <- function(rjmcmcressplit, indexM1, indexM2){
+  pvars <- dim(rjmcmcressplit$topmodel_res)[2]
+  selectorM1 <- c(1, rjmcmcressplit$topmodel_res[indexM1,(1:pvars-1)])==1
+  selectorM2 <- c(1, rjmcmcressplit$topmodel_res[indexM2,(1:pvars-1)])==1
+  dimM1 <- sum(selectorM1)-1
+  dimM2 <- sum(selectorM2)-1
+  
+  log_posterior_odds <- log(rjmcmcressplit$topmodel_res[indexM1,pvars]/rjmcmcressplit$topmodel_res[indexM2,pvars])  
+  log_bayesfactor <- log_posterior_odds - log(prior_prob_k_specific_model(dimM1, pvars-1, 1, 1)) + log(prior_prob_k_specific_model(dimM2, pvars-1, 1, 1))
+  return(log_bayesfactor)
+}
+
+
+
+ssplits = 2
+dataset_loaded <- f_dataset_loader("pima")
+splitted_data_rjmcmc <- f_pack_split_data(dataset_loaded$X, dataset_loaded$y, ssplits=ssplits, iseed=1, typesplit="random")
+splitted_data_rjmcmc <- f_prep_prior_logistic(splitted_data_rjmcmc, scale = 1)
+
+rjmcmc_split1 <- f_rjmcmc_on_splits(splitted_data_rjmcmc, i_split = 1, n.mil = 5)
+#rjmcmc_split2 <- f_rjmcmc_on_splits(splitted_data_rjmcmc, i_split = 2)
+
+
+
+
+indexM1 <- 4
+indexM2 <- 3
+pvars <- splitted_data_rjmcmc[[1]]$d
+
+f_bayesfactor_two_models_rjmcmc(rjmcmc_split1, indexM1, indexM2)
+
+
+selectorM1 <- c(1, rjmcmc_split1$topmodel_res[indexM1,(1:pvars-1)])
+selectorM2 <- c(1, rjmcmc_split1$topmodel_res[indexM2,(1:pvars-1)])
 
 source("~/R_programming/distbayesianmc/params_simulation/params_logit.R")
 stan_code <- readChar(fileName, file.info(fileName)$size)
@@ -73,15 +103,20 @@ mod <- stan_model(model_code = stan_code, auto_write = T)
 
 dataset_loaded <- f_dataset_loader("pima", nobs = nobs)
 
-splitted_dataM1 <- f_pack_split_data(dataset_loaded$X[,selectorM1==1], dataset_loaded$y, ssplits=1, iseed=1, typesplit="random")
+splitted_dataM1 <- f_pack_split_data(dataset_loaded$X[,selectorM1==1], dataset_loaded$y, ssplits=ssplits, iseed=1, typesplit="random")
 splitted_dataM1 <- f_prep_prior_logistic(splitted_dataM1, scale = 1)
-res_approx_full <-  f_stan_sampling_splitted_data(mod, splitted_dataM1, dataset = "pima", i_seed = 1, iter = 1, typesplit = "random", nchain = 10000, typeprior="normal")
-logbf_M1 <- res_approx_full$normconstcombined
+res_approx_M1 <-  f_stan_sampling_splitted_data(mod, splitted_dataM1, dataset = "pima", i_seed = 1, iter = 1, typesplit = "random", nchain = 10000, typeprior="normal")
+logbf_M1 <- res_approx_M1$normconstcombined
+res_approx_M1$vec_logsubpost
 
 
-splitted_dataM2 <- f_pack_split_data(dataset_loaded$X[,selectorM2==1], dataset_loaded$y, ssplits=1, iseed=1, typesplit="random")
+splitted_dataM2 <- f_pack_split_data(dataset_loaded$X[,selectorM2==1], dataset_loaded$y, ssplits=ssplits, iseed=1, typesplit="random")
 splitted_dataM2 <- f_prep_prior_logistic(splitted_dataM2, scale = 1)
-res_approx_small <-  f_stan_sampling_splitted_data(mod, splitted_dataM2, dataset = "pima", i_seed = 1, iter = 1, typesplit = "random", nchain = 10000, typeprior="normal")
-logbf_M2 <-  res_approx_small$normconstcombined
+res_approx_M2 <-  f_stan_sampling_splitted_data(mod, splitted_dataM2, dataset = "pima", i_seed = 1, iter = 1, typesplit = "random", nchain = 10000, typeprior="normal")
+logbf_M2 <-  res_approx_M2$normconstcombined
+res_approx_M2$vec_logsubpost
 
-logbf_M1 - logbf_M2   + log(prior_specific_model(sum(selectorM1)-1, 7, 1, 1))-log(prior_specific_model(sum(selectorM2)-1, 7, 1, 1))
+logbf_M1 - logbf_M2   + log(prior_prob_k_specific_model(sum(selectorM1)-1, 7, 1, 1)) - log(prior_prob_k_specific_model(sum(selectorM2)-1, 7, 1, 1))
+
+res_approx_M1$vec_logsubpost-res_approx_M2$vec_logsubpost
+
