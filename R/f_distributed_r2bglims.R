f33620ca82e9ef45f8fded01957840151175b5d0
Alexander Buchholz <alexander.buchholz@ensae.fr>
2019-08-07 16:11:27 +0100

    add systematic simulation

--- R/f_distributed_r2bglims.R
+++ R/f_distributed_r2bglims.R
@@ -48,16 +48,7 @@ prior_prob_k_specific_model <- function(dim_model, dim_all, a=1, b=1){
   return(part1/(part2 * choose(dim_all,dim_model))) # take into account that we are looking at exactly one single model
 }
 
-f_filter_rows_mcmc_rjmcmc_output <- function(mcmc_ouput, topmodel_res, index_model){
-  # filter selected colums
-  pvars <- dim(topmodel_res)[2]
-  selector_model <- c(1, topmodel_res[index_model,(1:pvars-1)])==1
-  mcmc_ouput_inter <- mcmc_ouput[,1:pvars]
-  filter_matrix <- mcmc_ouput_inter != 0
-  selector_rows <- apply(filter_matrix, 1, function(x) all(x==selector_model))
-  #selector_rows <- filter_matrix %*% selector_model == sum(selector_mode)
-  return(mcmc_ouput_inter[selector_rows, selector_model])
-}
+
 
 f_normal_approx_rjmcmc_output <- function(mcmc_ouput, topmodel_res, index_model){
   # first filter the output
@@ -75,9 +66,9 @@ f_bayesfactor_two_models_rjmcmc <- function(rjmcmcressplit, indexM1, indexM2){
   selectorM2 <- c(1, rjmcmcressplit$topmodel_res[indexM2,(1:pvars-1)])==1
   dimM1 <- sum(selectorM1)-1
   dimM2 <- sum(selectorM2)-1
-  
+  # check if the beta binomial prior is correct!
   log_posterior_odds <- log(rjmcmcressplit$topmodel_res[indexM1,pvars]/rjmcmcressplit$topmodel_res[indexM2,pvars])  
-  log_bayesfactor <- log_posterior_odds - log(prior_prob_k_specific_model(dimM1, pvars-1, 1, 1)) + log(prior_prob_k_specific_model(dimM2, pvars-1, 1, 1))
+  log_bayesfactor <- log_posterior_odds - log(prior_prob_k_specific_model(dimM1, pvars-1, 1, pvars-1)) + log(prior_prob_k_specific_model(dimM2, pvars-1, 1, pvars-1))
   return(log_bayesfactor)
 }
 
@@ -160,6 +151,7 @@ f_bf_simple_approx <- function(splitted_data_rjmcmc, results_topmodels, indexM1,
 
 
 f_simple_bf_two_models_all_splits <- function(list_params_model, results_topmodels, indexM1, indexM2){
+  #browser()
   scale <-  list_params_model$scale
   ssplits <-  list_params_model$ssplits
   typesplit <-  list_params_model$typesplit
@@ -167,10 +159,10 @@ f_simple_bf_two_models_all_splits <- function(list_params_model, results_topmode
   dataset_loaded <- f_dataset_loader(dataset)
   
   
-  pvars_all <- splitted_data_rjmcmc[[1]]$d  
-  pvars <- 1+dim(results_topmodels[[1]]$topmodel_res)[2]-3
-  selectorM1 <- c(1, as.numeric(results_topmodels[[1]]$topmodel_res[indexM1,(1:(pvars-1))])) # selection of included vars based on the first run of th sampler
-  selectorM2 <- c(1, as.numeric(results_topmodels[[1]]$topmodel_res[indexM2,(1:(pvars-1))]))
+  pvars_all <- dim(dataset_loaded$X)[2] #splitted_data_rjmcmc[[1]]$d  
+  pvars <- 1+dim(results_topmodels[[1]][[1]]$topmodel_res)[2]-3
+  selectorM1 <- c(1, as.numeric(results_topmodels[[1]][[1]]$topmodel_res[indexM1,(1:(pvars-1))])) # selection of included vars based on the first run of th sampler
+  selectorM2 <- c(1, as.numeric(results_topmodels[[1]][[1]]$topmodel_res[indexM2,(1:(pvars-1))]))
   
   source("~/R_programming/distbayesianmc/params_simulation/params_logit.R")
   stan_code <- readChar(fileName, file.info(fileName)$size)
@@ -190,5 +182,108 @@ f_simple_bf_two_models_all_splits <- function(list_params_model, results_topmode
   #logbf_M2 <-  res_approx_M2$normconstcombined
   #estimate_single <- logbf_M1 - logbf_M2   + log(prior_prob_k_specific_model(sum(selectorM1)-1, pvars_all-1, 1, pvars_all-1)) - log(prior_prob_k_specific_model(sum(selectorM2)-1, pvars_all-1, 1, pvars_all-1))  
   return(list(M1 = res_approx_M1, M2 = res_approx_M2))
+}
+
+f_full_run_rep_rjmcmc <- function(list_params_model){
+  dataset_loaded <- f_dataset_loader(list_params_model$dataset)
+  splitted_data_rjmcmc <- f_pack_split_data(dataset_loaded$X, dataset_loaded$y, ssplits=list_params_model$ssplits, iseed=1, typesplit=list_params_model$typesplit, dataset = list_params_model$dataset_loaded$dataset)
+  splitted_data_rjmcmc <- f_prep_prior_logistic(splitted_data_rjmcmc, scale = list_params_model$scale)
+  #i_split <-  1
+  mrep <- list_params_model$mrep
+  # repeat the sampling on one single split
+  results_splits <- list()
+  results_splits_topmodel_frame <- list()
+  for(i_split in 1:list_params_model$ssplits){
+    results_topmodels_parallel <- f_parallel_repeat_rjmcmc_sampling(mrep, splitted_data_rjmcmc, i_split = i_split, n.mil = list_params_model$n.mil, ncores = 6, savestring = "test")
+    results_splits[[i_split]] <- results_topmodels_parallel
+    df_sim_res <- f_combine_topmodels_in_df(results_topmodels_parallel)
+    df_sim_res[["split"]] <- i_split
+    results_splits_topmodel_frame[[i_split]] <- df_sim_res
+  }
+  df_sim_res_all <-  do.call(rbind, results_splits_topmodel_frame)
+  return(list(results_splits=results_splits, df_sim_res_all = df_sim_res_all))
+}
+
+f_filter_rows_mcmc_rjmcmc_output <- function(mcmc_output, topmodel_res, key_model){
+  # filter selected colums
+  pvars <- dim(topmodel_res)[2]-2 # remove key model and counter sim
+  selector_model <- c(1, topmodel_res[topmodel_res$key_model == key_model,(1:pvars-1)])==1
+  mcmc_ouput_inter <- mcmc_output[,1:pvars]
+  filter_matrix <- mcmc_ouput_inter != 0
+  selector_rows <- apply(filter_matrix, 1, function(x) all(x==selector_model))
+  #selector_rows <- filter_matrix %*% selector_model == sum(selector_mode)
+  return(mcmc_ouput_inter[selector_rows, selector_model])
+}
+
+f_summary_stats_per_split <- function(key, res_split_list){
+  results_splits <- res_split_list$results_splits
+  splits <- length(results_splits)
+  mrep <- length(results_splits[[1]])
+  summary_stats_list <- list()
+  pvars <- sum(unlist(strsplit(key, "")) == "1") + 1 # number of variables
+  
+  
+  summary_stats_list <-  list()
+  for(i_rep in 1:mrep){
+    # loop over repetitions
+    mat_means <- matrix(0, ncol=pvars, nrow=splits)
+    mat_cov <- array(0, dim=c(pvars, pvars, splits))
+    summary_stats_list[[i_rep]] <- list()
+    # loop over the splits
+    for(i_split in 1:splits){
+      mcmc_output_inter <- f_filter_rows_mcmc_rjmcmc_output(results_splits[[i_split]][[i_rep]]$mcmc_ouput, results_splits[[i_split]][[i_rep]]$topmodel_res, key) 
+      mat_means[i_split,] <- colMeans(mcmc_output_inter)
+      mat_cov[,,i_split] <- cov(mcmc_output_inter)
+    }
+    summary_stats_list[[i_rep]]$mat_means <- mat_means
+    summary_stats_list[[i_rep]]$mat_cov <- mat_cov
+  }
+  return(summary_stats_list) # first layer: splits, second layer iterations
+}
+
+f_joint_bf_model_splits_rjmcmc <- function(res_onesplit, res_several_splits, key1, key2){
+  mrep <- length(res_onesplit$results_splits[[1]])
+  bfonesplit <- rep(0, mrep)
+  
+  # extract means and variance of splits running over repetions
+  summary_stats_list1 <- f_summary_stats_per_split(key1, res_several_splits)
+  summary_stats_list2 <- f_summary_stats_per_split(key2, res_several_splits)
+  
+  bfseveralsplits <- rep(0, mrep)
+  
+  for(i_rep in 1:mrep){
+    # single split
+    postprob1 <- df1 %>% filter(key_model == key1 | key_model == key2) %>% filter(counter_sim == i_rep) %>% select(Post.Prob) 
+    bfonesplit[i_rep] <- log(postprob1[1,]/postprob1[2,])
+    
+    # several splits
+    M1dim <- sum(unlist(strsplit(key1, "")) == "1") + 1 # number of variables
+    M2dim <- sum(unlist(strsplit(key2, "")) == "1") + 1 # number of variables
+    fulldim_no_intercept <- nchar(key1)
+    
+    df2 %>% filter(key_model == key1 | key_model == key2) %>% filter(counter_sim == i_rep) %>% select(Post.Prob, split, key_model)
+    df2_key1 <- df2 %>% filter(key_model == key1) %>% filter(counter_sim == 1) %>% select(Post.Prob, split)
+    df2_key2 <- df2 %>% filter(key_model == key2) %>% filter(counter_sim == 1) %>% select(Post.Prob, split)
+    bfsplits <- df2_key1$Post.Prob/df2_key2$Post.Prob
+    
+    log_alpha_key1 <- list_params_model_twosplits$ssplits*f_alpha_sub(ssplits = list_params_model_twosplits$ssplits, Vprior = diag(rep(list_params_model_twosplits$scale, M1dim)), typeprior = "normal")
+    log_alpha_key2 <- list_params_model_twosplits$ssplits*f_alpha_sub(ssplits = list_params_model_twosplits$ssplits, Vprior = diag(rep(list_params_model_twosplits$scale, M2dim)), typeprior = "normal")
+    
+    log_priorM1 <- log(prior_prob_k_specific_model(M1dim-1, fulldim_no_intercept, 1, fulldim_no_intercept))
+    log_priorM2 <- log(prior_prob_k_specific_model(M2dim-1, fulldim_no_intercept, 1, fulldim_no_intercept))
+    log_prior_odds <- log_priorM1 - log_priorM2
+    
+    prod1 <- f_integral_product_gaussian(summary_stats_list1[[i_rep]]$mat_means, summary_stats_list1[[i_rep]]$mat_cov)
+    prod2 <- f_integral_product_gaussian(summary_stats_list2[[i_rep]]$mat_means, summary_stats_list2[[i_rep]]$mat_cov)
+    
+    
+    log_bf_splits <- sum(log(bfsplits)) - log_prior_odds + log_alpha_key1 - log_alpha_key2 + prod1 - prod2
+    #log(bffull)
+    
+    bfseveralsplits[i_rep] <- log_bf_splits
+  }
+  return(list(bfonesplit=bfonesplit, bfseveralsplits=bfseveralsplits))
+  
+  
 }
 
