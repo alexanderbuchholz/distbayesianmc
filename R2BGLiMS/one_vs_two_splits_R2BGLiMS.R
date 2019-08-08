495a9e57ba08740f9ffb89f875ee3f934ff4e0af
Alexander Buchholz <alexander.buchholz@ensae.fr>
2019-08-08 14:22:33 +0100

    try to find the bug

--- R2BGLiMS/one_vs_two_splits_R2BGLiMS.R
+++ R2BGLiMS/one_vs_two_splits_R2BGLiMS.R
@@ -12,12 +12,12 @@ list_params_model_onesplit <- list(scale =  1,
                           ncores = 4)
 
 list_params_model_multisplits1 <- list(scale =  10,
-                                   ssplits =  2,
+                                   ssplits =  3,
                                    typesplit =  "random",
                                    dataset =  "pima",#"",
                                    mrep = 12, 
-                                   n.mil = 5, 
-                                   ncores = 4)
+                                   n.mil = 2, 
+                                   ncores = 6)
 
 
 
@@ -36,12 +36,19 @@ df1 <- res_multisplit1$df_sim_res_all
 #df2 <- res_twosplits$df_sim_res_all
 #keys <- df1[df1$counter_sim==1,]$key_model
 
-dim(df1[df1$counter_sim==1,])
+f_intersection_keys <- function(splits, df){
+  keys_per_split1 <- df %>% filter(counter_sim == 1, split==1) %>% dplyr::select(key_model)
+  common_keys <- intersect(keys_per_split1, keys_per_split1)    
+  for(i_split in 2:splits){
+    keys_per_split_i <- df %>% filter(counter_sim == 1, split==i_split) %>% dplyr::select(key_model)
+    common_keys <- intersect(common_keys, keys_per_split_i)    
+  }
+  return(common_keys)
+}
 
 
-keys_per_split1 <- df1 %>% filter(counter_sim == 1, split==1) %>% dplyr::select(key_model)
-keys_per_split2 <- df1 %>% filter(counter_sim == 1, split==2) %>% dplyr::select(key_model)
-common_keys <- intersect(keys_per_split1, keys_per_split2)
+
+common_keys <- f_intersection_keys(list_params_model_multisplits1$ssplits, df1)
 
 i_split <- 2
 i_rep <- 3
@@ -52,6 +59,7 @@ index_M2 <- 2
 key1 <- common_keys[index_M1,]
 key2 <- common_keys[index_M2,]
 
+
 log(mean( (df1 %>%  filter((key_model == key1  ) & split == i_split) %>% dplyr::select(Post.Prob))[,]  ) / 
   mean((df1 %>%  filter((key_model == key2 ) & split == i_split) %>% dplyr::select(Post.Prob))[,] ) )
 
@@ -61,7 +69,7 @@ res_simple_comp <- f_simple_bf_two_models_all_splits(list_params_model_multispli
 
 estimate_single <- res_simple_comp$M1$vec_logsubpost - res_simple_comp$M2$vec_logsubpost + res_simple_comp$M1$model_prior - res_simple_comp$M2$model_prior
 
-colMeans(res_simple_comp$M1$betasamples[[2]])
+colMeans(res_simple_comp$M1$betasamples[[i_split]])
 cov(res_simple_comp$M1$betasamples[[2]])
 summary_stats_list1 <- f_summary_stats_per_split(key1, res_multisplit1)
 summary_stats_list1[[2]]$mat_means
