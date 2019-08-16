5a730d36cc6cb04bdd4d95bf02ce44663f3dcf02
alexanderbuchholz <alexanderbuchholz@yahoo.de>
2019-08-16 12:31:12 +0200

    fix bug

--- scripts_r/rscript_rjmcmc.R
+++ scripts_r/rscript_rjmcmc.R
@@ -7,7 +7,7 @@ print(args)
 
 sim_id <- as.numeric(args[1])
 
-mrep <- 24
+mrep <- 20
 n.mil <- 2
 n.cores <- 1
 dataset <-  "sim2"
@@ -80,7 +80,7 @@ for(params_model in list_params_model){
   #set.seed(sim_id)
   #x_wait <- rexp(1,1)
   #Sys.sleep(x_wait)
-  foreach(sim_id = 1:iters) %dopar% {
+  foreach(sim_id = 1:mrep) %dopar% {
     res_sim <- f_single_run_rep_rjmcmc(params_model, sim_id)
   }
   #save(res_sim, file = paste("res_split_id", sim_id, "_splits_", params_model$ssplits, ".RData", sep = ""))
