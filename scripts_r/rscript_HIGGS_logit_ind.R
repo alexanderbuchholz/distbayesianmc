612397e075798da274da4bb006d63a47ef89afaf
alexanderbuchholz <alexanderbuchholz@yahoo.de>
2019-08-16 11:15:32 +0200

    remove dso

--- scripts_r/rscript_HIGGS_logit_ind.R
+++ scripts_r/rscript_HIGGS_logit_ind.R
@@ -32,13 +32,13 @@ for (dataset in vec_datasets) {
             print("Try exception: recompile model and run again")
             source("~/R_programming/distbayesianmc/params_simulation/params_logit_higgs.R")
             stan_code <- readChar(fileName, file.info(fileName)$size)
-            mod <- stan_model(model_code = stan_code)
+            mod <- stan_model(model_code = stan_code, save_dso=FALSE)
             print("compiled, now running")
             f_stan_sampling_single_split(mod, splitted_data[[i_split]], dataset = dataset, i_seed = i_iter, iter = i_iter, typesplit = typesplit, nchain = nchain, typeprior = typeprior)
             print("Run finished")
           }
         )
-    print(tempdir())
+    print(tempdir()) 
   }
 }
 
