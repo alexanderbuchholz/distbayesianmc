ea84477a9968bb28718ec4c438003f582162ec12
Alexander Buchholz <alexander.buchholz@ensae.fr>
2019-08-20 11:16:59 +0100

    eval rjmcmc for higgs

--- sim_results/scripts_analyse_results/evaluate_rjmcmc.R
+++ sim_results/scripts_analyse_results/evaluate_rjmcmc.R
@@ -3,9 +3,10 @@
 library(distbayesianmc)
 
 typesplit <-  "random"
-#setwd("/scratch/alexander/distbayesianmc_rjmcmc/")
+setwd("/scratch/alexander/distbayesianmc_rjmcmc/")
 source("~/R_programming/distbayesianmc/scripts_r/rscript_rjmcmc.R")
-setwd("~/R_programming/exchange_files_server/rjmcmc_sim2/")
+
+#setwd("~/R_programming/exchange_files_server/rjmcmc_sim2/")
 #files_sim_results <- list.files()
 
 #selector_files <- grepl(dataset, files_sim_results)
@@ -15,9 +16,13 @@ setwd("~/R_programming/exchange_files_server/rjmcmc_sim2/")
 #res_out$mcmc_ouput
 
 # putting all the model results in one joint frame
-vec_splits <- c(1,2,3,5,10)
-dataset <- "sim2"
+vec_splits <- c(1,2,3,5)#,10)
+dataset <- "higgs1_large"
 iters <-  20
+
+# use this only for the higgs data set
+list_params_model <- list_params_model[1:4]
+
 list_all_results_df <- list()
 list_all_results_output <- list()
 list_all_results_all <- list()
@@ -49,12 +54,13 @@ for (ssplits in vec_splits){
 df_all <-  do.call(rbind, list_all_results_df)
 
 common_keys_single <- f_intersection_keys(list_params_model_onesplit$ssplits, list_all_results_df[['1']])#, df_single_split)
+
 for (params_model in list_params_model){
   common_keys_multisplit <- f_intersection_keys(params_model$ssplits, list_all_results_df[[as.character(params_model$ssplits)]])#, df_single_split)
   keys_intersect <- intersect(common_keys_single, common_keys_multisplit)
 }
 
-combos <- list(c(1,2), c(2,3), c(3,4), c(1,3), c(2,4), c(1,4))
+combos <- list(c(1,2))#, c(2,3), c(3,4), c(1,3), c(2,4), c(1,4))
 list_res <- list()
 counter_combs <- 0
 for(model_comb in combos){
